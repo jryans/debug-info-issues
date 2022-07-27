1a62ed700d28132a5f33e9be6ea82c055d2b6f0e
J. Ryan Stinnett <jryans@gmail.com>
2022-07-27 12:11:54 +0100

    Add structure to run scripts

--- KLEE/loop-iters-from-input/run.sh
+++ KLEE/loop-iters-from-input/run.sh
@@ -1,13 +1,16 @@
 set -eux
 
+KLEE="${HOME}/Projects/klee/build-release/bin/klee"
+COMMON_OPTS="--debug-print-instructions=all:file --use-query-log=all:kquery,solver:kquery --debug-execution-trace --max-forks=4"
+
 rm -rf klee-out-O0
-~/Projects/klee/build-release/bin/klee --debug-print-instructions=all:file --use-query-log=all:kquery,solver:kquery --debug-execution-trace --output-dir=klee-out-O0 --max-forks=4 example-O0.bc
+${KLEE} ${COMMON_OPTS} --output-dir=klee-out-O0  example-O0.bc
 
 rm -rf klee-out-O1-partial-good
-~/Projects/klee/build-release/bin/klee --debug-print-instructions=all:file --use-query-log=all:kquery,solver:kquery --debug-execution-trace --output-dir=klee-out-O1-partial-good --max-forks=4 example-O1-partial-good.bc
+${KLEE} ${COMMON_OPTS} --output-dir=klee-out-O1-partial-good example-O1-partial-good.bc
 
 rm -rf klee-out-O1-partial-bad
-~/Projects/klee/build-release/bin/klee --debug-print-instructions=all:file --use-query-log=all:kquery,solver:kquery --debug-execution-trace --output-dir=klee-out-O1-partial-bad --max-forks=4 example-O1-partial-bad.bc
+${KLEE} ${COMMON_OPTS} --output-dir=klee-out-O1-partial-bad example-O1-partial-bad.bc
 
 rm -rf klee-out-O1
-~/Projects/klee/build-release/bin/klee --debug-print-instructions=all:file --use-query-log=all:kquery,solver:kquery --debug-execution-trace --output-dir=klee-out-O1 --max-forks=4 example-O1.bc
+${KLEE} ${COMMON_OPTS} --output-dir=klee-out-O1 example-O1.bc
