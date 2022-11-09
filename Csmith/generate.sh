635c2f20be73da2dbb62e7fd788813a02674d575
J. Ryan Stinnett <jryans@gmail.com>
2022-11-09 18:23:45 +0000

    Add Csmith example generator

---
+++ Csmith/generate.sh
@@ -0,0 +1,15 @@
+set -eux
+
+csmith \
+--no-argc \
+--no-checksum \
+--no-float \
+--max-block-depth 2 \
+--max-block-size 2 \
+--max-expr-complexity 2 \
+--max-funcs 2 \
+--no-safe-math \
+--no-builtins \
+> example.c
+
+replace --silent '\"csmith.h\"' '<stdint.h>' example.c
