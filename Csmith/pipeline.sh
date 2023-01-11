ffd18b76c7f3a15c632812f02248ac2837505313
J. Ryan Stinnett <jryans@gmail.com>
2023-01-11 17:05:29 +0000

    Fix pipeline stream truncation

--- Csmith/pipeline.sh
+++ Csmith/pipeline.sh
@@ -1,7 +1,9 @@
-set -eux
+set -eu
 
 JOBS="${1:-4}"
 OUTPUT_DIR="experiment-$(date '+%Y-%m-%dT%H:%M')"
 
 mkdir ${OUTPUT_DIR}
-seq ${JOBS} | parallel -j -2 --bar --joblog "${OUTPUT_DIR}/summary" --wd "${OUTPUT_DIR}/{}/" --results "${OUTPUT_DIR}/{}/" ../../job.sh
+# `script` is used to fake a terminal, which tweaks stream buffering and ensures
+# everything displays as it would interactively.
+seq ${JOBS} | parallel -j -2 --bar --joblog "${OUTPUT_DIR}/summary" --wd "${OUTPUT_DIR}/{}/" --results "${OUTPUT_DIR}/{}/" script -q results ../../job.sh
