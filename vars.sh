187d0d234d404588387f7cb8a0cbb1664ed613b7
J. Ryan Stinnett <jryans@gmail.com>
2023-01-16 18:22:11 +0000

    Eagerly expand paths in root vars

--- vars.sh
+++ vars.sh
@@ -3,30 +3,27 @@ set -eux
 llvm() {
   local build=$1
   local program=$2
-  shift 2
-  $HOME/Projects/LLVM/llvm/build-$build/bin/$program "$@"
+  echo "$HOME/Projects/LLVM/llvm/build-$build/bin/$program"
 }
 
 klee() {
   local build=$1
   local program=$2
-  shift 2
-  $HOME/Projects/klee/build-$build/bin/$program "$@"
+  echo "$HOME/Projects/klee/build-$build/bin/$program"
 }
 
 csmith() {
   local build=$1
   local program=$2
-  shift 2
-  $HOME/Projects/csmith/build-$build/src/$program "$@"
+  echo "$HOME/Projects/csmith/build-$build/src/$program"
 }
 
-CSMITH="csmith release csmith"
+CSMITH=$(csmith release csmith)
 
 SRC_FILE="example.c"
 
-CLANG="llvm release-clang-lldb-13.0.0 clang"
-LLVM_AS="llvm release-clang-lldb-13.0.0 llvm-as"
+CLANG=$(llvm release-clang-lldb-13.0.0 clang)
+LLVM_AS=$(llvm release-clang-lldb-13.0.0 llvm-as)
 
 CC_COMMON_OPTS="-I ${HOME}/Projects/klee/include -g -fno-inline -fno-discard-value-names -Xclang -disable-O0-optnone"
 CC_IR_OPTS="-S -emit-llvm"
@@ -41,9 +38,9 @@ filter_cg_ir() {
   < ${input_ir} grep -v '^+' | sed '2,/IR Dump/!d' | grep -v 'IR Dump' > ${output_ir}
 }
 
-PRINT_MODULE="klee debug print-module"
+PRINT_MODULE=$(klee debug print-module)
 
-KLEE="klee release klee"
+KLEE=$(klee release klee)
 KLEE_COMMON_OPTS="--output-module --use-query-log=all:kquery,solver:kquery --debug-execution-trace --max-forks=4"
 
 O0_BC="klee-out-O0/final.bc"
@@ -52,5 +49,5 @@ O1_FIXED_BC="klee-out-O1-fixed/final.bc"
 O2_BC="klee-out-O2/final.bc"
 O2_FIXED_BC="klee-out-O2-fixed/final.bc"
 
-CHECK="klee debug debug-info-check"
+CHECK=$(klee debug debug-info-check)
 CHECK_OPTS="--debug-only=debug-info-check,independent-function,values-collector,variable --debug-execution-trace"
