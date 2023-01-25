34f1253bdf7f91ae20b04b3deb552fff6c4d8144
J. Ryan Stinnett <jryans@gmail.com>
2023-01-25 15:15:27 +0000

    Rewrap pipeline script

--- Csmith/pipeline.sh
+++ Csmith/pipeline.sh
@@ -6,4 +6,11 @@ OUTPUT_DIR="experiment-$(date '+%Y-%m-%dT%H:%M')"
 mkdir ${OUTPUT_DIR}
 # `script` is used to fake a terminal, which tweaks stream buffering and ensures
 # everything displays as it would interactively.
-seq ${JOBS} | parallel -j -2 --bar --joblog "${OUTPUT_DIR}/summary" --wd "${OUTPUT_DIR}/{}/" --results "${OUTPUT_DIR}/{}/" script -q results ../../job.sh
+seq ${JOBS} | \
+parallel -j -2 \
+--bar \
+--joblog "${OUTPUT_DIR}/summary" \
+--wd "${OUTPUT_DIR}/{}/" \
+--results "${OUTPUT_DIR}/{}/" \
+script -q results \
+../../job.sh
