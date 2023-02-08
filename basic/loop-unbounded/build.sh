e3db391db138e5be49c4ef4955cc6c6457f973cc
J. Ryan Stinnett <jryans@gmail.com>
2023-02-08 21:41:39 +0000

    Update older build scripts to match common check script

--- basic/loop-unbounded/build.sh
+++ basic/loop-unbounded/build.sh
@@ -3,28 +3,31 @@ set -eux
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${SCRIPT_DIR}/../vars.sh"
 
+mkdir -p klee-out-O0
 ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_IR_OPTS} ${CC_O0_OPTS} -o example-O0.ll
-${LLVM_AS} example-O0.ll
-# ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_O0_OPTS} ${CC_LINK_OPTS} -o example-O0
+${LLVM_AS} -o klee-out-O0/final.bc example-O0.ll
 ${OPT} ${OPT_CFG_OPTS} example-O0.ll
 mv .example.dot example-O0.dot
 
 # IndVarSimplifyPass removes loop debug values with Clang 13.0.0
 # Limit below stops just before this
+mkdir -p klee-out-O1-partial-good
 ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_IR_OPTS} ${CC_O1_OPTS} -mllvm -opt-bisect-limit=47 -o example-O1-partial-good.ll
-${LLVM_AS} example-O1-partial-good.ll
+${LLVM_AS} -o klee-out-O1-partial-good/final.bc example-O1-partial-good.ll
 
 # This advances one more pass to include the bad IndVarSimplifyPass
+mkdir -p klee-out-O1-partial-bad
 ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_IR_OPTS} ${CC_O1_OPTS} -mllvm -opt-bisect-limit=48 -o example-O1-partial-bad.ll
-${LLVM_AS} example-O1-partial-bad.ll
+${LLVM_AS} -o klee-out-O1-partial-bad/final.bc example-O1-partial-bad.ll
 
 ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_IR_OPTS} ${CC_O1_OPTS} -o example-O1.ll
-${LLVM_AS} example-O1.ll
-# ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_O1_OPTS} ${CC_LINK_OPTS} -o example-O1
 
+mkdir -p klee-out-O1
 ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_CG_IR_OPTS} ${CC_O1_OPTS} -o /dev/null 2> example-O1-cg-raw.ll
 filter_cg_ir example-O1-cg-raw.ll example-O1-cg.ll
 rm example-O1-cg-raw.ll
-${LLVM_AS} example-O1-cg.ll
+${LLVM_AS} -o klee-out-O1/final.bc example-O1-cg.ll
 ${OPT} ${OPT_CFG_OPTS} example-O1-cg.ll
 mv .example.dot example-O1-cg.dot
+
+# ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_O1_OPTS} ${CC_LINK_OPTS} -o example-O1
