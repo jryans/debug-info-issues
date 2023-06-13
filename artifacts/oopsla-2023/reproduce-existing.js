8c201a6543d4bfc8da0656071edff47509644662
J. Ryan Stinnett <jryans@gmail.com>
2023-06-13 14:21:03 +0100

    Rename LLVM example to earlycse-removes-arguments

--- artifacts/oopsla-2023/reproduce-existing.js
+++ artifacts/oopsla-2023/reproduce-existing.js
@@ -17,7 +17,7 @@ const dirs = [
   "LLVM/simplifycfg-select-unreachable-line-2",
   "LLVM/simplifycfg-removes-unused-value",
   "LLVM/localutils-salvages-loads",
-  "LLVM/localutils-removes-arguments",
+  "LLVM/earlycse-removes-arguments",
 ];
 
 const total = dirs.length;
