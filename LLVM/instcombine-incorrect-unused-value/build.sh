ff76f9c61596d68003de6a152d6bed70093896a9
J. Ryan Stinnett <jryans@gmail.com>
2023-06-21 17:24:28 +0100

    Remove unused compilation to binary in LLVM examples

--- LLVM/instcombine-incorrect-unused-value/build.sh
+++ LLVM/instcombine-incorrect-unused-value/build.sh
@@ -5,12 +5,9 @@ source "${SCRIPT_DIR}/../vars.sh"
 
 mkdir -p klee-out-O0
 ${LLVM_AS} -o klee-out-O0/final.bc example-O0.ll
-${LLC} -O0 -o example-O0.o --filetype obj example-O0.ll
 
 mkdir -p klee-out-O2
 ${LLVM_AS} -o klee-out-O2/final.bc example-O2.ll
-${LLC} -O0 -o example-O2.o --filetype obj example-O2.ll
 
 mkdir -p klee-out-O2-fixed
 ${LLVM_AS} -o klee-out-O2-fixed/final.bc example-O2-fixed.ll
-${LLC} -O0 -o example-O2-fixed.o --filetype obj example-O2-fixed.ll
