3a36264b4793998bf29ae100eadad54870b57d75
J. Ryan Stinnett <jryans@gmail.com>
2024-03-16 17:57:00 +0000

    Tweak build to also print input module

--- basic/single-path-multiple-assignments/build.sh
+++ basic/single-path-multiple-assignments/build.sh
@@ -6,6 +6,7 @@ source "${SCRIPT_DIR}/../vars.sh"
 mkdir -p klee-out-O0
 ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_IR_OPTS} ${CC_O0_OPTS} -o example-O0.ll
 ${LLVM_AS} -o klee-out-O0/final.bc example-O0.ll
+${PRINT_MODULE} klee-out-O0/final.bc > klee-out-O0/final.ll
 
 ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_IR_OPTS} ${CC_O1_OPTS} -o example-O1.ll
 
@@ -14,5 +15,6 @@ ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_CG_IR_OPTS} ${CC_O1_OPTS} -o /dev/nu
 filter_cg_ir example-O1-cg-raw.ll example-O1-cg.ll
 rm example-O1-cg-raw.ll
 ${LLVM_AS} -o klee-out-O1/final.bc example-O1-cg.ll
+${PRINT_MODULE} klee-out-O1/final.bc > klee-out-O1/final.ll
 
 # ${CLANG} ${SRC_FILE} ${CC_COMMON_OPTS} ${CC_O1_OPTS} ${CC_LINK_OPTS} -o example-O1
