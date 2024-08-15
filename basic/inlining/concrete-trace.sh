e0491c56a7c552afa1e111157bf4b0a5f8692750
J. Ryan Stinnett <jryans@gmail.com>
2024-08-15 15:08:12 +0100

    Record verbose concrete trace as well

--- basic/inlining/concrete-trace.sh
+++ basic/inlining/concrete-trace.sh
@@ -7,6 +7,11 @@ mkdir -p concrete-trace-O0
 (
   cd concrete-trace-O0;
   # TODO: Generalise this to support Linux as well as macOS
+  CON_TRACE_VERBOSE=1 \
+    DYLD_INSERT_LIBRARIES=${CON_COLLECT_INSTRUMENTATION} \
+    "$@" \
+    ../example-O0 \
+    0
   DYLD_INSERT_LIBRARIES=${CON_COLLECT_INSTRUMENTATION} \
     "$@" \
     ../example-O0 \
@@ -17,6 +22,11 @@ mkdir -p concrete-trace-O1
 (
   cd concrete-trace-O1;
   # TODO: Generalise this to support Linux as well as macOS
+  CON_TRACE_VERBOSE=1 \
+    DYLD_INSERT_LIBRARIES=${CON_COLLECT_INSTRUMENTATION} \
+    "$@" \
+    ../example-O1 \
+    0
   DYLD_INSERT_LIBRARIES=${CON_COLLECT_INSTRUMENTATION} \
     "$@" \
     ../example-O1 \
