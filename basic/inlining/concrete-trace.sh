a65b6c833038c4c97516ba7eba84843ba20f32b1
J. Ryan Stinnett <jryans@gmail.com>
2024-08-22 14:07:35 +0100

    Add event source to inlining traces

--- basic/inlining/concrete-trace.sh
+++ basic/inlining/concrete-trace.sh
@@ -8,11 +8,13 @@ mkdir -p concrete-trace-O0
   cd concrete-trace-O0;
   env \
     CON_TRACE_VERBOSE=1 \
+    CON_TRACE_SOURCE=1 \
     ${CON_COLLECT_INSTRUMENTATION} \
     "$@" \
     ../example-O0 \
     0
   env \
+    CON_TRACE_SOURCE=1 \
     ${CON_COLLECT_INSTRUMENTATION} \
     "$@" \
     ../example-O0 \
@@ -24,11 +26,13 @@ mkdir -p concrete-trace-O1
   cd concrete-trace-O1;
   env \
     CON_TRACE_VERBOSE=1 \
+    CON_TRACE_SOURCE=1 \
     "${CON_COLLECT_INSTRUMENTATION}" \
     "$@" \
     ../example-O1 \
     0
   env \
+    CON_TRACE_SOURCE=1 \
     ${CON_COLLECT_INSTRUMENTATION} \
     "$@" \
     ../example-O1 \
